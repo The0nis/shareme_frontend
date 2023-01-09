5233b559a93f1db03b7c2b7dace4d61450468211
TheOnis <onisamuelmichael@gmail.com>
2023-01-09 19:20:54 +0000

    done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,18 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import { RiHomeFill } from "react-icons/ri";
-import { IoIosArrowForward, IOIOsArrowForward } from "react-icons/io";
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+import logo from '../assets/logo.png';
+import { categories } from '../utils/data';
 
-import logo from "../assets/logo.png";
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
 
-const isNotActiveStyle =
-  "flex items-center px-5 gap-3 text-gray-500 hover:text-black transistion-all  duration-200 ease-in-out capitalize";
-
-const isActiveStyle =
-  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transistion-all  duration-200 ease-in-out capitalize";
-
-const categories = [
-  {
-    name: "Animals",
-  },
-  {
-    name: "Wallpapers",
-  },
-  {
-    name: "Photography",
-  },
-  {
-    name: "Gaming",
-  },
-  {
-    name: "Coding",
-  },
-  {
-    name: "Other",
-  },
-];
-const Sidebar = ({ user, closeToggle }) => {
+const Sidebar = ({ closeToggle, user }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   };
+
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
       <div className="flex flex-col">
